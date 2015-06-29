cc9fc461963cc46817914364a774254a4908e6b5
Ryan Holmes <holmes.ryan.90@gmail.com>
2015-06-29 11:36:37 -0400

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -1,10 +1,14 @@
+"""
+https://projecteuler.net/problem=4
+
+Very straightforward. For this solution, we calculate all possible values and
+test if they are palindromic by reversing the string. Keep the number if it is
+our highest
+"""
 max = 999
 min = 100
 result = 0
 
-# We basically calculate all possible values, keeping
-# our highest value in a variable
-# At the end of the loops, just spit the value out
 for x in xrange(max, min, -1):
     for y in xrange(max, min, -1):
         product = x*y
