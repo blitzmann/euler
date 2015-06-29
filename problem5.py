698cb613fe0b7d523797b20e950aaea5e94ce743
Ryan Holmes <holmes.ryan.90@gmail.com>
2015-06-29 11:42:51 -0400

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -1,18 +1,25 @@
 '''
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+https://projecteuler.net/problem=5
 
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-
-Start with the examples answer. Our final result must be a multiple of this answer (otherwise it would not satisfy the 1-10), which allows us to jump through a large amount of integers to test. We can also eliminate the first 10 checks as they would be inherently validated by the use of our multiple
+There's a trick to this one. We already know that 2520 is the lowest number
+evenly divided by 1-10. We use this fact for two purposes:
+- Since our result must be evenly divided by 1-10 (and 11-20), the result must
+    be a multiple of 2520 (otherwise it would not satisfy 1-10 condition).
+- Since our tests are multiples of 2520, we do not need to test 1-10 as they are
+    inherently valid
+Both of these facts greatly speed up the process when compared to brute-forcing
+it.
 '''
 
 m = 2520
 x = m
 while True:
-    for i in xrange(10,20):
+    for i in xrange(10, 20):
         if x % (i+1) != 0:
+            # break the for loop and continue with the while
             break
     else:
+        # break the while loop when we find a satisfactory answer
         break
     x += m
 print x
