026f455132d6e5bf1d664552a66f72f0ffbb709b
Ryan Holmes <holmes.ryan.90@gmail.com>
2015-07-05 15:10:01 -0400

    Create problem79.py

---
+++ problem79.py
@@ -0,0 +1,7 @@
+"""
+https://projecteuler.net/problem=79
+
+I did this problem by hand, shouldn't be difficult to implement in code. 
+Basically search for all digists and see where it fit in between two 
+other digits. Very easy. Maybe I'll get around to scripting it eventually
+"""
