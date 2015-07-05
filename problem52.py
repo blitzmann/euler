75423405c20dfe49fd41a34c7b66901787a6aeaf
Ryan Holmes <holmes.ryan.90@gmail.com>
2015-07-05 14:21:06 -0400

    Update problem52.py

--- problem52.py
+++ problem52.py
@@ -4,14 +4,13 @@ https://projecteuler.net/problem=52
 We calculate 2x and store the digits in a set. Then we calculate through 3x-6x
 and compare those sets to our 2x. Pretty straightforward
 """
-
 n=1
 while True:
     n += 1
 
-    s2 = set(list(str(n*2)))
+    s2 = set(str(n*2))
     for x in xrange(3, 7):
-        if s2 != set(list(str(n*x))):
+        if s2 != set(str(n*x)):
             break
     else:
         break
