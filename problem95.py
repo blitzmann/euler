3fdd0400931fa65b10b7c499937d30e56de9f647
Ryan Holmes <holmes.ryan.90@gmail.com>
2015-07-05 21:42:23 -0400

    Create problem95.py

---
+++ problem95.py
@@ -0,0 +1,26 @@
+"""
+https://projecteuler.net/problem=92
+
+We create a recursive function that computes the square sum and returns true if 
+it's 89. Takes over a minute to compute all < 10,000,000, but it works 
+"""
+end = 10000000
+n = 0
+
+def square_sum(n):
+    s = sum(map(lambda x: int(x)**2, list(str(n))))
+    if s == 1:
+        return False
+    if s == 89:
+        return True
+    return square_sum(s)
+
+r = 0
+while n < end:
+    if n % 1000 == 0:
+        print n
+    n += 1
+    if square_sum(n):
+        r += 1
+
+print r
